0d8f5029f8b63cf95596c70f6d8a73791e3b0dca
MichaelLeonardo <michaelleonardo69@gmail.com>
2022-04-09 21:18:08 +0700

    Menambahkan Function lihat list All buku by genre

--- Controllers/book_controllers.go
+++ Controllers/book_controllers.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 )
 
 func LookAllBookList(w http.ResponseWriter, r *http.Request) {
@@ -13,6 +14,39 @@ func LookAllBookList(w http.ResponseWriter, r *http.Request) {
 	defer db.Close()
 
 	query := "SELECT * FROM buku"
+
+	rows, err := db.Query(query)
+	if err != nil {
+		log.Println(err)
+	}
+
+	var book Buku
+	var books []Buku
+
+	for rows.Next() {
+		if err := rows.Scan(&book.Isbn, &book.Judul, &book.Penulis, &book.Edisi, &book.TahunCetak, &book.Harga); err != nil {
+			log.Fatal(err)
+		} else {
+			books = append(books, book)
+		}
+	}
+
+	var response ArrBukuResponse
+
+	response.Data = books
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func LookAllBookListFilterByGenre(w http.ResponseWriter, r *http.Request) {
+	db := Connect()
+	defer db.Close()
+
+	vars := mux.Vars(r)
+	idGenre := vars["id_genre"]
+
+	query := ("SELECT * from buku a join genrebuku b on a.isbn = b.isbn WHERE b.id_genre = " + idGenre)
 
 	rows, err := db.Query(query)
 	if err != nil {
