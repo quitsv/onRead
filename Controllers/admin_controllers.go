cff49dcd64d1dacb203dc551489390932aad1c78
quitsv <axellsilvano29@gmail.com>
2022-04-09 13:51:14 +0700

    add controller for delete book

--- Controllers/admin_controllers.go
+++ Controllers/admin_controllers.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 )
 
 func AddNewBook(w http.ResponseWriter, r *http.Request) {
@@ -35,5 +36,34 @@ func AddNewBook(w http.ResponseWriter, r *http.Request) {
 		} else {
 			PrintError(400, "Tambah buku gagal", w)
 		}
+	}
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	db := Connect()
+	defer db.Close()
+
+	err := r.ParseForm()
+	if err != nil {
+		log.Print(err)
+		PrintError(400, "Error ParseForm", w)
+		return
+	}
+
+	vars := mux.Vars(r)
+	isbn := vars["isbn"]
+
+	result, errQuery := db.Exec("delete from buku where isbn = ?", isbn)
+
+	num, _ := result.RowsAffected()
+
+	if errQuery == nil {
+		if num != 0 {
+			PrintSuccess(200, "Hapus buku berhasil", w)
+		} else {
+			PrintError(400, "Tidak ada buku ditemukan", w)
+		}
+	} else {
+		PrintError(400, "Hapus buku gagal", w)
 	}
 }
