8c432d29df31b07df493b9b6f4ba781a9254dc1a
quitsv <axellsilvano29@gmail.com>
2022-04-09 15:11:03 +0700

    add controller for delete user

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ func main() {
 	router.HandleFunc("/books", Controllers.AddNewBook).Methods("POST")          //add new book
 	router.HandleFunc("/books/{isbn}", Controllers.DeleteBook).Methods("DELETE") //delete book
 	router.HandleFunc("/books/{isbn}", Controllers.UpdateBook).Methods("PUT")    //update book
+	router.HandleFunc("/pengguna", Controllers.DeleteUser).Methods("DELETE")     //delete user
 
 	//cors
 	corsHandler := cors.New(cors.Options{
