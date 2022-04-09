56f529487431f0eac415ae7fcbe7ea2efff88eb3
quitsv <axellsilvano29@gmail.com>
2022-04-09 14:17:28 +0700

    add controller for update book

--- Controllers/admin_controllers.go
+++ Controllers/admin_controllers.go
@@ -67,3 +67,39 @@ func DeleteBook(w http.ResponseWriter, r *http.Request) {
 		PrintError(400, "Hapus buku gagal", w)
 	}
 }
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	db := Connect()
+	defer db.Close()
+
+	err := r.ParseForm()
+	if err != nil {
+		log.Print(err)
+		PrintError(400, "Error ParseForm", w)
+		return
+	}
+
+	vars := mux.Vars(r)
+	isbn := vars["isbn"]
+
+	isbn_new := r.Form.Get("isbn")
+	judul := r.Form.Get("judul")
+	penulis := r.Form.Get("penulis")
+	edisi := r.Form.Get("edisi")
+	tahun_cetak := r.Form.Get("tahun_cetak")
+	harga := r.Form.Get("harga")
+
+	result, errQuery := db.Exec("update buku set isbn = ?, judul = ?, penulis = ?, edisi = ?, tahun_cetak = ?, harga = ? where isbn = ?", isbn_new, judul, penulis, edisi, tahun_cetak, harga, isbn)
+
+	num, _ := result.RowsAffected()
+
+	if errQuery == nil {
+		if num != 0 {
+			PrintSuccess(200, "Update buku berhasil", w)
+		} else {
+			PrintError(400, "Tidak ada buku ditemukan", w)
+		}
+	} else {
+		PrintError(400, "Update buku gagal", w)
+	}
+}
